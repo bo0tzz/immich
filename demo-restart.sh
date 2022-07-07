46e22a2a7239a77e32fd1192f3d556f74e74c75d
bo0tzz <btdewilligen@gmail.com>
2022-07-07 06:38:03 +0000

    Add demo-restart script to repo

---
+++ demo-restart.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -ex
+
+cd /home/ubuntu/immich
+sudo iptables -A INPUT -p tcp --dport 80 -j DROP
+
+docker-compose -f docker/docker-compose.yml down -v
+docker-compose -f docker/docker-compose.yml up -d immich-server
+
+until curl 127.0.0.1:3001/server-info/ping; do sleep 1; done
+
+curl --location --request POST '127.0.0.1:3001/auth/admin-sign-up' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "email": "demo@immich.app",
+    "password": "demo",
+    "firstName": "John",
+    "lastName": "Doe"
+}'
+
+docker-compose -f docker/docker-compose.yml up -d
+
+sudo iptables -D INPUT -p tcp --dport 80 -j DROP
